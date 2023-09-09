b9ca2b6584158a2b37c1ca9caf8dffd698cf6244
andriibessarab <andriibessarab@gmail.com>
2023-09-08 21:22:06 -0400

    Implement react router into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import Layout from "./pages/Layout";
+// import "./styles/reset.scss";
+import "./styles/global.scss";
+
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="app-container">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout/>}>
+                        <Route index element={<HomePage/>}/>
+                        <Route path="register" element={<RegisterPage/>}/>
+                        <Route path="login" element={<LoginPage/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
