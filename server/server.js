6d5c40bc3a4ce887a6054b697b15c95390a37069
andriibessarab <andriibessarab@gmail.com>
2023-10-14 01:10:44 -0400

    Expand the initial server setup by adding CORS configuration, request parsing for JSON and form data, and routing for '/api/users'. Also, synchronize the database models, with the option to remove 'force: true' for production

--- server/server.js
+++ server/server.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const cors = require('cors');
+const db = require("./db");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+let corsOptions = {
+    origin: "http://localhost:5000"
+};
 
+app.use(cors(corsOptions));
+
+// Parse requests of content-type - application/json
+app.use(express.json());
+
+// Parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: false}));
+
+app.use('/api/users', require('./routes/api/users'));
+
+db.sync({force: true}) // Note: remove force: true for production
+    .then(() => {
+        console.log("Synced models.");
+    })
+    .catch((err) => {
+        console.log("Failed to sync models: " + err.message);
+    });
+
+// Set port, listen for requests
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
